dced4d39126941bce746eb4e2416b8c4a6adce7a
Lyrecoph <lyrecoph30@gmail.com>
2024-10-08 22:28:52 +0100

    configuration of the database and implementation of the database structure

--- store/models.py
+++ store/models.py
@@ -1,3 +1,56 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# Table client
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete= models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+"""Le modèle de produit sera composé d'un "nom", "prix" et numérique(digital). 
+   Le numérique sera simplement vrai ou faux et nous indiquera s'il s'agit d'un 
+   produit numérique ou d'un produit physique qui doit être expédié.
+"""
+
+# Table produit
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+    
+# Table commande
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+    
+# Table article commander(Panier)
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+# Table Addresse de livraison
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
